7a391ef3e24bb12e986f69350ce58bbae7351000
Shadi Jaafar <shadinjaafar@gmail.com>
2021-10-03 22:41:24 -0400

    Added nav-bar image

--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import sheet from './style.css';
 import card from './create-card';
 import { clickSubmit } from './submit-button';
 import { clickAddTask } from './task-list';
+import image from './just-do-it.jpg';
 
 const submitButton = document.getElementById('submit-button');
 const addButton = document.getElementById('add-button');
 const xButton = document.getElementById('x-button');
+const nav = document.querySelector('nav');
+
+const navImage = new Image();
+navImage.src = image;
+navImage.classList.add('image');
+nav.appendChild(navImage);
 
 submitButton.onclick = clickSubmit;
 
